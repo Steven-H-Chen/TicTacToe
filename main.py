949dbd5ba55767fd978808f4d7a18d03de4f1cec
Steven Chen <134465360+Steven-H-Chen@users.noreply.github.com>
2024-04-05 20:46:17 -0700

    Program Completed
    Need to include win declaration
    Need to update tile UI
    Need to fix position of restart button and label

--- main.py
+++ main.py
@@ -13,8 +13,42 @@ class MyTicApp(App):
     def build(self):
         return Layoutsx()
     
-    def presser(presser, btn):
-       pass
+    turn = "X"
+    def presser(self, btn):
+        if self.turn == 'X':
+            btn.text ="X"
+            btn.disabled = True
+            self.root.ids.score.text = "0's Turn"
+            self.turn ="O"
+        else:
+            btn.text ="O"
+            btn.disabled = True 
+            self.root.ids.score.text = "X's Turn"
+            self.turn = "X"
+
+
+    def restart(self):
+        self.turn = "X"
+
+        self.root.ids.btn1.disabled = False
+        self.root.ids.btn2.disabled = False
+        self.root.ids.btn3.disabled = False
+        self.root.ids.btn4.disabled = False
+        self.root.ids.btn5.disabled = False
+        self.root.ids.btn6.disabled = False
+        self.root.ids.btn7.disabled = False
+        self.root.ids.btn8.disabled = False
+        self.root.ids.btn9.disabled = False
+
+        self.root.ids.btn1.text = ""
+        self.root.ids.btn2.text = ""
+        self.root.ids.btn3.text = ""
+        self.root.ids.btn4.text = ""
+        self.root.ids.btn5.text = ""
+        self.root.ids.btn6.text = ""
+        self.root.ids.btn7.text = ""
+        self.root.ids.btn8.text = ""
+        self.root.ids.btn9.text = ""
     
 if __name__ == '__main__':
     MyTicApp().run()
